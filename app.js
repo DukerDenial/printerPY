45985177c6ac46e7e727814088acf86445943810
Tokan <tokan@gmail.com>
2026-04-01 09:28:42 +0300

    GO1

---
+++ app.js
@@ -0,0 +1,40 @@
+const API = "https://YOUR_SERVER_IP:5000";
+
+async function loadPrinters() {
+    const res = await fetch(API + "/printers");
+    const data = await res.json();
+
+    const container = document.getElementById("printers");
+    container.innerHTML = "";
+
+    data.forEach(p => {
+        const div = document.createElement("div");
+        div.className = "card";
+
+        let html = `<b>${p.name}</b><br>`;
+
+        p.carts.forEach(c => {
+            html += `
+            ${c.name} (${c.qty})
+            <button class="btn plus" onclick="update(${c.id}, '+')">+</button>
+            <button class="btn minus" onclick="update(${c.id}, '-')">-</button>
+            <br>
+            `;
+        });
+
+        div.innerHTML = html;
+        container.appendChild(div);
+    });
+}
+
+async function update(id, act) {
+    await fetch(API + "/update", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({id, act})
+    });
+
+    loadPrinters();
+}
+
+loadPrinters();
